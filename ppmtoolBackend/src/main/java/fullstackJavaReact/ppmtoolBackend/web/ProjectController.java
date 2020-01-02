622924432b90b134c2f83264c779c62f03756368
Evan Kim <dkim2435@gmail.com>
2020-01-02 18:34:24 -0500

    added valid annotation and BindingResult in ResponseEntity of the Project controller in web package then created if statement to return custom error string and HttpStatus of 400 Bad Request

--- ppmtoolBackend/src/main/java/fullstackJavaReact/ppmtoolBackend/web/ProjectController.java
+++ ppmtoolBackend/src/main/java/fullstackJavaReact/ppmtoolBackend/web/ProjectController.java
@@ -1,8 +1,11 @@
 package fullstackJavaReact.ppmtoolBackend.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +29,12 @@ public class ProjectController {
 	//Create a new route that we can POST our new project.
 	@PostMapping("")
 	// ResponseEntity lets us have control to our JSON objects.
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	// Adding @Valid will display 400 status if the object created is invalid
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+		}
+		
 		// saves the project object to the database
 		// make sure when testing in postman, id is auto generated.
 		Project project1 = projectService.saveOrUpdateProject(project);
