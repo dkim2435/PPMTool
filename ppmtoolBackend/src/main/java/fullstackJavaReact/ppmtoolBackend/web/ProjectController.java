248606b5bcc6544bbd2ccf9bc36faf68674686a7
Evan Kim <dkim2435@gmail.com>
2020-01-02 20:33:48 -0500

    In the Project Controller, Map and HashMap is used along with forloop to cherry pick field and errorMessage key value from List<FieldError> to customize the error message that is displayed when 400 Bad Request happens

--- ppmtoolBackend/src/main/java/fullstackJavaReact/ppmtoolBackend/web/ProjectController.java
+++ ppmtoolBackend/src/main/java/fullstackJavaReact/ppmtoolBackend/web/ProjectController.java
@@ -1,11 +1,16 @@
 package fullstackJavaReact.ppmtoolBackend.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,8 +37,17 @@ public class ProjectController {
 	// Adding @Valid will display 400 status if the object created is invalid
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		if(result.hasErrors()) {
-			return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+			
+			// only pull out "field" and "errorMessage" from List<FieldError> and make our own error message
+			Map<String, String> errorMap = new HashMap<>();
+			for(FieldError error: result.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			
+			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
 		}
+		
+
 		
 		// saves the project object to the database
 		// make sure when testing in postman, id is auto generated.
