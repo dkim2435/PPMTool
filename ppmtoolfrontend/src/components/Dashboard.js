7f1f3ddefcd87a44bc9c77ee17c3e8345f06394d
Evan Kim <dkim2435@gmail.com>
2020-01-06 16:52:22 -0500

    created dashboard component and tested to see how components work

---
+++ ppmtoolfrontend/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// class based components are components that can take props/properties
+
+class Dashboard extends Component {
+  render() {
+    return <h1>Welcome to the Dashboard</h1>;
+  }
+}
+
+export default Dashboard;
