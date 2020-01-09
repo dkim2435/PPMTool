4fc3b37c589d0300342918df738181b4a0372c9c
Evan Kim <dkim2435@gmail.com>
2020-01-09 11:37:54 -0500

    added ProjectItem.js inside the components folder which will be used to render different project items inside of the dashboard. And also created Layout folder that has the header.js which will be used as a navbar

--- ppmtoolfrontend/src/components/Dashboard.js
+++ ppmtoolfrontend/src/components/Dashboard.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 // class based components are components that can take props/properties
 
 class Dashboard extends Component {
   render() {
-    return <h1>Welcome to the Dashboard</h1>;
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+
+        <ProjectItem />
+
+        <ProjectItem />
+      </div>
+    );
   }
 }
 
