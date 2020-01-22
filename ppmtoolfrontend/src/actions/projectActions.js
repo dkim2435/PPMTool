b032dea4d66a218c0ac23035c3909fc9b3ab4c97
Evan Kim <dkim2435@gmail.com>
2020-01-22 09:15:48 -0500

    type.js to create action type, errorReducer.js to create reducer for that action, went into index.js to combine it to the rootReducer, used axios in projectActions.js to post the project object to the backend server, went into addProject.js to bind it to the submit button, added @CrossOrigin import in the backend contoller layer due to cross origin error

---
+++ ppmtoolfrontend/src/actions/projectActions.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { GET_ERRORS } from "./types";
+
+// history is what is going to allow to redirect once the form is submitted (redirect to dashboard)
+// dispatch is what we're going to be returning
+// async = function will return a promise
+// await = wait til promise is rendered
+export const createProject = (project, history) => async dispatch => {
+  try {
+    const res = await axios.post("http://localhost:8080/api/project", project);
+    history.push("/dashboard");
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
+
+//summary: createProject function will return a promise(async).
+//try posting to the backend server with project obj parameter and if it works, take the user back to the dashboard
+// or else catch the error by dispatching GET_ERRORS reducer
